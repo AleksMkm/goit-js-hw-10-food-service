396f44a2887b1f5920fde84c94597b1b0b962bee
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2020-10-05 21:07:27 +0300

    adds menu markup render

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles.css';
+
+import menuTemplate from './templates/menu-items.hbs';
+import menu from './menu.json';
+
+const menuEl = document.querySelector('.js-menu');
+const menuMarkup = menuTemplate(menu);
+
+menuEl.insertAdjacentHTML('beforeend', menuMarkup);
